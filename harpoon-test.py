aec316c0abb93c0d018fa8cc257b7abf0e591da6
Minwoo Yu <snowypainter@gmail.com>
2024-11-28 23:57:53 +0900

    iamges

--- harpoon-test.py
+++ harpoon-test.py
@@ -1,15 +1,9 @@
 import utils
 import harpoon_model
-import backtester
+import os
 import matplotlib.pyplot as plt
 import numpy as np
-
-symbol = "SMCI"
-symbols = [symbol]
-
-data = utils.load_historical_for_learning(symbol, utils.today_before(900), utils.today(), interval='1d')
-
-model = harpoon_model.finetune_model(symbol, harpoon_model.calculate_technical_indicators(data, symbol))
+from tensorflow.keras.models import load_model
 
 def detect_and_plot_signals():
     """
@@ -23,31 +17,39 @@ def detect_and_plot_signals():
     v_signals = []
     inverted_v_signals = []
     prices = data[f"{symbol}_Price"].values
-
     bar = 0
     while bar < len(data) - harpoon_model.seqlen:
         if bar > harpoon_model.seqlen * 2:
             start = bar - harpoon_model.seqlen - 49 # must be 49.
             end = bar
             pred = harpoon_model.predict(model, data.iloc[start:end], symbol)
-
             if np.argmax(pred) == 1:  # 역V자 패턴
                 inverted_v_signals.append(bar)
             elif np.argmax(pred) == 2:  # V자 패턴
                 v_signals.append(bar)
         
         bar += 1
-
     # 시각화
     plt.figure(figsize=(12, 6))
     plt.plot(prices, label=f"{symbol} Price", color="blue")
-    plt.scatter(v_signals, prices[v_signals], color="green", label="V Pattern", marker="^", alpha=0.7)
-    plt.scatter(inverted_v_signals, prices[inverted_v_signals], color="red", label="Inverted V Pattern", marker="v", alpha=0.7)
+    plt.scatter(v_signals, prices[v_signals], color="green", label="V Pattern", marker="^", alpha=1, s=70)  # Increased size
+    plt.scatter(inverted_v_signals, prices[inverted_v_signals], color="red", label="Inverted V Pattern", marker="v", alpha=1, s=70)  # Increased size
     plt.title(f"Price Chart with V and Inverted V Patterns: {symbol}")
     plt.xlabel("Time (Bar)")
     plt.ylabel("Price")
     plt.legend()
     plt.grid(True)
-    plt.show()
+    plt.savefig(f"{symbol}_v_inverted_v_patterns.png")  # Save the figure as an image
+    plt.close()  # Close the plot to free up memory
+
+
+for symbol in ["TSLA", "NVDA", "SMCI"]:
+    symbols = [symbol]
+    data = utils.load_historical_for_learning(symbol, utils.today_before(900), utils.today(), interval='1d')
+    if os.path.exists(f"best_HARPOON_{symbol}_finetuned_model.h5"):
+        print(f"Model for {symbol} already exists. Skipping training.")
+        model = load_model(f"best_HARPOON_{symbol}_finetuned_model.h5")
+    else:
+        model = harpoon_model.finetune_model(symbol, harpoon_model.calculate_technical_indicators(data, symbol))
     
-detect_and_plot_signals()
+    detect_and_plot_signals()
