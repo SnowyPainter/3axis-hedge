a5c4abf485aee5c886c76d2b6216f4d6f2815f9c
Minwoo Yu <snowypainter@gmail.com>
2024-12-21 11:28:09 +0900

    Upload to hf

---
+++ upload_to_hf.py
@@ -0,0 +1,21 @@
+from huggingface_hub import upload_file
+
+with open('./hftoken.txt', 'r') as file:
+    HF_TOKEN = file.readline().strip()
+REPO_ID = "SnowyPainter/AlgoShield"
+REPO_TYPE = "model"
+LOCAL_FILES = [
+    "gap/test.joblib"
+]
+for local_file in LOCAL_FILES:
+    try:
+        upload_file(
+            path_or_fileobj=local_file,
+            path_in_repo=local_file,
+            repo_id=REPO_ID,
+            repo_type=REPO_TYPE,
+            token=HF_TOKEN,
+        )
+        print(f"Uploaded {local_file} successfully!")
+    except Exception as e:
+        print(f"Failed to upload {local_file}: {e}")
